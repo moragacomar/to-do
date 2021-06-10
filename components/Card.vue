152dea46a92627c4c065ec1884a671d89e1882bf
Juan Moraga <poluxfc@gmail.com>
2021-06-10 18:20:03 -0300

    Lista de tareas

---
+++ components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    :class="stated ? 'bg-green-200' : 'bg-blue-100'"
+    class="rounded-md container h-500 p-1 flex justify-between my-2"
+  >
+    <div class="p-5 border-l-4 border-blue-500 text-blue-500 text-sm font-bold">
+      {{ title }}
+    </div>
+    <div class="actions flex items-center">
+      <button
+        :class="!stated ? 'bg-green-400' : 'bg-red-400'"
+        class="text-white rounded-full px-3 h-8 mx-1 focus:outline-none"
+        @click="$emit('update')"
+      >
+        {{ !stated ? "&#10003;" : "&#215;" }}
+      </button>
+      <button
+        class="
+          bg-red-500
+          rounded-full
+          text-white
+          px-3
+          h-8
+          mx-1
+          focus:outline-none
+        "
+        @click="$emit('delete')"
+      >
+        &#215;
+      </button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    title: { type: String, default: "" },
+    stated: { type: Boolean, default: false },
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="m-2">
+    <div class="p-1">
+      <div class="relative flex items-center">
+        <div class="absolute top-0 mr-0 right-0 h-full p-2 flex items-center">
+          <button
+            @click="agregarTarea"
+            class="
+              rounded-full
+              mr-0
+              bg-blue-500
+              h-full h-8
+              w-8
+              text-lg text-white text-bold
+              hover:bg-blue-600
+              focus:outline-none
+            "
+          >
+            +
+          </button>
+        </div>
+        <input
+          v-model="title"
+          class="
+            py-2
+            pl-5
+            pr-12
+            focus:outline-none
+            border-2
+            w-full
+            rounded-full
+            border-grey-300
+          "
+          type="text"
+          name="title"
+          id="title"
+        />
+      </div>
+    </div>
+    <Card
+      @update="item.stated = !item.stated"
+      :stated="item.stated"
+      @delete="BorrarTarea(i)"
+      :title="item.title"
+      :key="i"
+      v-for="(item, i) in tarea"
+    />
+  </div>
+</template>
+
+<script>
+import Card from "~/components/Card.vue";
+export default {
+  components: { Card },
+  data() {
+    return {
+      tarea: [],
+      title: "",
+    };
+  },
+  methods: {
+    agregarTarea() {
+      if (this.title.trim()) {
+        this.tarea.push({ title: this.title.toUpperCase(), stated: false });
+        this.title = "";
+      }
+    },
+    BorrarTarea(p) {
+      this.tarea.splice(p, 1);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
